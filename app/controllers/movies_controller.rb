3b3a0b001d4741f7ef94bfd854ec1c4fd634ccbe
anmolthakur31 <anmolthakur110@gmail.com>
2017-04-26 12:27:19 +0530

    random

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,9 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
-
+qwertyuiop[]
+qwertyuio
+qwertyuio[pojhgfdsa]
+ddddddddddddddddddddddddd
   # GET /movies
   # GET /movies.json
   def index
